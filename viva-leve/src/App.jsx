49aef168b3993543ceb58ea8085d76b26437102c
mateus <mateusdev23@gmail.com>
2025-07-22 18:37:15 -0300

    Concluindo Calculador IMC

--- viva-leve/src/App.jsx
+++ viva-leve/src/App.jsx
@@ -1,13 +1,54 @@
-import './App.css'
+import { data } from "./data/data.js"
+
+import { useState } from 'react'
+
 import ImcCalc from './components/ImcCalc'
+import ImcTable from './components/ImcTable'
+
+import './App.css'
 
 function App() {
+
+  const [imc, setImc] = useState("");
+  const [info, setInfo] = useState("");
+  const [infoClass, setInfoClass] = useState("");
+
+  const calcImc = (e, heigth, weigth) => {
+    e.preventDefault();
+        
+    if(!weigth || !heigth) return;
+
+    const weigthFloat = +weigth.replace(",", ".");
+    const heigthFloat = +heigth.replace(",", ".")
+
+    const imcResult = (weigthFloat / (heigthFloat * heigthFloat)).toFixed(1);
+
+    setImc(imcResult);
+
+    data.forEach((item) => {
+      if(imcResult >= item.min && imcResult <= item.max) {
+        setInfo(item.info);
+        setInfoClass(item.infoClass);
+      }
+    });
+
+    if(!info) return;
+  }
+
+  const resetCalc = (e) => {
+    e.preventDefault();
+
+    setImc("");
+    setInfo("");
+    setInfoClass("");
+
+  }
 
   return (
     <>
       <div className="container">
         {/* <h1>Viva Leve</h1> */}
-        <ImcCalc/>
+        {!imc ? <ImcCalc calcImc={calcImc}/> : <ImcTable data={data} imc={imc} info={info} infoClass={infoClass} resetCalc={resetCalc}/>}
       </div>
     </>
   )
--- viva-leve/src/components/ImcCalc.jsx
+++ viva-leve/src/components/ImcCalc.jsx
@@ -1,36 +1,69 @@
-import './ImcCalc.css'
-const ImcCalc = () => {
-  return (
-    <div>
-        <h2>Calculadora de IMC</h2>
-        <form id='imc-form'>
-            <div className="form-inputs">
-                <div className="form-control">
-                    <label htmlFor="heigth">Altura:</label>
-                    <input 
-                        type="text" 
-                        name='heigth' 
-                        id='height' 
-                        placeholder='1,75'
-                    />
+import { useState } from 'react';
+
+import Button from './Button';
+
+import './ImcCalc.css';
+
+const ImcCalc = ({ calcImc }) => {
+
+    const [height, setHeigth] = useState("");
+    const [weigth, setWeigth] = useState("");
+
+    const clearForm = (e) => {
+        e.preventDefault();
+        setHeigth(""); // Limpandos os dados de Altura
+        setWeigth(""); // Limpandos os dados de Peso
+    }
+
+    const validDigits = (text) => {
+        return text.replace(/[^0-9,]/g, "")
+    }
+
+    const handleHeigthChange = (e) => {
+        const updateValue = validDigits(e.target.value)
+        setHeigth(updateValue);
+    }
+
+    const handleWeigthChange = (e) => {
+        const updateValue = validDigits(e.target.value)
+        setWeigth(updateValue);
+    }
+
+    return (
+        <div id='calc-container'>
+            <h2>Calculadora de IMC</h2>
+            <form id='imc-form'>
+                <div className="form-inputs">
+                    <div className="form-control">
+                        <label htmlFor="heigth">Altura:</label>
+                        <input 
+                            type="text" 
+                            name='heigth' 
+                            id='height' 
+                            placeholder='Exemplo 1,75'
+                            onChange={(e) => handleHeigthChange(e)}
+                            value={height}
+                        />
+                    </div>
+                    <div className="form-control">
+                        <label htmlFor="weight">Peso:</label>
+                        <input 
+                            type="text" 
+                            name='weight' 
+                            id='weight' 
+                            placeholder='Exemplo 70,5'
+                            onChange={(e) => handleWeigthChange(e)}
+                            value={weigth}
+                        />
+                    </div>
+                    <div className="action-control">
+                        <Button id="clear-btn" text="Limpar" action={clearForm}/>
+                        <Button id="calc-btn" text="Calcular" action={(e) => calcImc(e, height, weigth)}/>
+                    </div>
                 </div>
-                <div className="form-control">
-                    <label htmlFor="weight">Peso:</label>
-                    <input 
-                        type="text" 
-                        name='weight' 
-                        id='weight' 
-                        placeholder='70,5'
-                    />
-                </div>
-                <div className="action-control">
-                    <button>Calcular</button>
-                    <button>Limpar</button>
-                </div>
-            </div>
-        </form>
-    </div>
-  )
+            </form>
+        </div>
+    )
 }
 
 export default ImcCalc
