d96c263a82999d56c1a1d5b53122ad02da50c9db
Mateusinhodev <mateusdev23@gmail.com>
2025-08-05 13:42:48 -0300

    Concluindo o Conteúdo Main

--- viva-leve/src/pages/Main/index.jsx
+++ viva-leve/src/pages/Main/index.jsx
@@ -5,26 +5,45 @@ import "./main.css"
 export default function Main() {
     return (
         <section className="main-container">
-            <div className="calculadoras">
-                <h2 className="calculadoras-title">Calculadoras de Saúde</h2>
-                <button className="calculadoras-btn">IMC</button>
-                <button className="calculadoras-btn">TMB</button>
-                <button className="calculadoras-btn">Gasto Calórico Diário</button>
-                <button className="calculadoras-btn">Água Recomendada</button>
+            <div className="container-left">
+                <div className="calculadoras">
+                    <h2 className="calculadoras-title">Calculadoras de Saúde</h2>
+                    <button className="calculadoras-btn">IMC</button>
+                    <button className="calculadoras-btn">TMB</button>
+                    <button className="calculadoras-btn">Gasto Calórico Diário</button>
+                    <button className="calculadoras-btn">Água Recomendada</button>
+                </div>
+                <div class="jornada-content">
+                    <h2>Minha Jornada</h2>
+    <p>Alguns lembretes simples para te ajudar a cuidar de você com leveza:</p>
+
+    <ul>
+      <li>Estabeleça metas reais e possíveis.</li>
+      <li>Pese-se no máximo 1 vez por semana.</li>
+      <li>Observe como você se sente (não só o peso).</li>
+      <li>Anote seus hábitos e pequenas vitórias.</li>
+      <li>Cuide da água, do sono e da respiração.</li>
+    </ul>
+
+    <p>Quer acompanhar seu progresso?</p>
+    <a href="/downloads/Diario_Viva_Leve.pdf" class="btn-download" download>Baixar Diário em PDF</a>
+                </div>
             </div>
-            <div className="profissionais">
-                <div className="profissionais-card">
-                    <div className="card-title">
-                        <h2>Encontre ajuda profissional</h2>
-                    </div>
-                    <div className="card-img">
-                        <img src={imgMain} alt="Imagem de doutora"/>
-                    </div>
-                    <div className="action">
-                        <a href="profissionais">Buscar profissionais de Sáude</a>
+            
+            <div className="container-rigth">
+                <div className="profissionais">
+                    <div className="profissionais-card">
+                        <div className="card-title">
+                            <h2>Encontre ajuda profissional</h2>
+                        </div>
+                        <div className="card-img">
+                            <img src={imgMain} alt="Imagem de doutora"/>
+                        </div>
+                        <div className="action">
+                            <a href="profissionais">Buscar profissionais de Sáude</a>
+                        </div>
                     </div>
                 </div>
-                
             </div>
         </section>
         
